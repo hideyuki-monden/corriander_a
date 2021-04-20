625f08ecb0ce0ae2d6120aa17bb27dfdbe9fca70
hideyuki_monden <s11ec400@yahoo.co.jp>
2021-04-20 13:19:24 +0900

    a

---
+++ android/app/src/main/kotlin/hideyuki/corriandar_a_app/MainActivity.kt
@@ -0,0 +1,6 @@
+package hideyuki.corriandar_a_app
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,45 @@
+import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+
+import 'main_model.dart';
+
+void main() => runApp(MyApp());
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      home: ChangeNotifierProvider<MainModel>(
+        create: (_) => MainModel(),
+        child: Scaffold(
+          appBar: AppBar(
+            title: Text('コリアンダー'),
+          ),
+          body: Consumer<MainModel>(builder: (context, model, child) {
+              return Center(
+                child: Column(
+                  children: [
+                    Text(
+                      model.kboyText,
+                      style: TextStyle(
+                        fontSize:30,
+                      ),
+                          ),
+                    ElevatedButton(
+                      child: Text('ボタン'),
+                        onPressed: () {
+                      //ここでなにか
+                      model.changeKboyText();
+                      },
+                    ),
+                  ],
+                ),
+              );
+            }
+          ),
+            ),
+      ),
+    );
+  }
+}
